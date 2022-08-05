f8a6dd897b7bcbd733a04eddd7267b4d5e020c6c
quadiez <dduidokdoi@gmail.com>
2022-08-05 14:47:07 +0700

    feat: chang bg

--- src/products.ts
+++ src/products.ts
@@ -4,14 +4,20 @@ export const products = [
         variants: [
             {
                 color: "#B0AEAF",
+                startColor: "rgba(182, 180, 181, 0.7) 1.49%",
+                endColor: "rgba(194, 192, 193, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/hoodie/gray.png`,
             },
             {
                 color: "#682934",
+                startColor: "rgba(110, 47, 58, 0.7) 1.49%",
+                endColor: "rgba(108, 45, 56, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/hoodie/red.png`,
             },
             {
                 color: "#292F41",
+                startColor: "rgba(46, 51, 70, 0.7) 1.49%",
+                endColor: "rgba(41, 46, 65, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/hoodie/black.png`,
             },
         ],
@@ -21,14 +27,20 @@ export const products = [
         variants: [
             {
                 color: "#E46091",
+                startColor: "rgba(208, 87, 132, 0.7) 1.49%",
+                endColor: "rgba(222, 94, 141, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/tank/pink.png`,
             },
             {
                 color: "#FFB39D",
+                startColor: "rgba(253, 178, 157, 0.7) 1.49%",
+                endColor: "rgba(252, 179, 157, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/tank/orange.png`,
             },
             {
                 color: "#6881C5",
+                startColor: "rgba(105, 131, 200, 0.7) 1.49%",
+                endColor: "rgba(108, 132, 200, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/tank/blue.png`,
             },
         ],
@@ -61,14 +73,20 @@ export const products = [
         variants: [
             {
                 color: "#B0AEAF",
+                startColor: "rgba(182, 180, 181, 0.7) 1.49%",
+                endColor: "rgba(194, 192, 193, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/hoodie/gray.png`,
             },
             {
                 color: "#682934",
+                startColor: "rgba(110, 47, 58, 0.7) 1.49%",
+                endColor: "rgba(108, 45, 56, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/hoodie/red.png`,
             },
             {
                 color: "#292F41",
+                startColor: "rgba(46, 51, 70, 0.7) 1.49%",
+                endColor: "rgba(41, 46, 65, 0.2) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/hoodie/black.png`,
             },
         ],
@@ -96,14 +114,20 @@ export const products = [
         variants: [
             {
                 color: "#23AD5B",
+                startColor: "#229656 2.6%",
+                endColor: "#98E7BE 99.6%",
                 src: `${process.env.PUBLIC_URL}/images/product/dip/green.png`,
             },
             {
                 color: "#6AD1F0",
+                startColor: "rgba(17, 150, 207, 0.8) 1.49%",
+                endColor: "rgba(151, 228, 248, 0.66) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/dip/blue.png`,
             },
             {
                 color: "#E43878",
+                startColor: "rgba(217, 47, 96, 0.8) 1.49%",
+                endColor: "rgba(253, 170, 198, 0.6) 102.78%",
                 src: `${process.env.PUBLIC_URL}/images/product/dip/pink.png`,
             },
         ],
